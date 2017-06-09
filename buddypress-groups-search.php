7e31c88d7ef3aa0462547fa645e4c53dec873ee3
Pallavi Gupta <pallavi@wbcomdesigns.com>
2017-06-09 13:32:15 +0530

    updating readme and screenshots

--- buddypress-groups-search.php
+++ buddypress-groups-search.php
@@ -3,7 +3,7 @@
   Plugin Name: BuddyPress Group Type Search
   Plugin URI: https://wbcomdesigns.com/contact/
   Description: This plugin allows site visitors to search for BuddyPress groups that includes searching based on buddypress group types in the site.
-  Version: 1.0.1
+  Version: 1.0.2
   Author: Wbcom Designs
   Author URI: http://wbcomdesigns.com
   License: GPLv2+
@@ -63,14 +63,27 @@ function bpgts_group_type_search_plugin_init() {
             deactivate_plugins(plugin_basename(__FILE__));
             wp_die(__('The <b>BuddyPress Group Type Search</b> plugin requires <b>BuddyPress Add Group Types</b> plugin to be installed and active. Return to <a href="' . admin_url('plugins.php') . '">Plugins</a>', 'bp-profile-filter'));
         }
-    } else{
+    } else {
       // If BuddyPress && Create Group Type plugin is NOT active
-      $cg_active = in_array('bp-create-group-type/bp-add-group-types.php', get_option('active_plugins'));
+      $bp_cg_active = in_array('bp-create-group-type/bp-add-group-types.php', get_option('active_plugins'));
+	  $boddypress_cg_active = in_array('buddypress-create-group-type-master/bp-add-group-types.php', get_option('active_plugins'));
+	  
+	  //check for both folders GitHub and Wordpress.org
+	  if($bp_cg_active === true || $boddypress_cg_active === true)
+		  $cg_active = true;
+	  
       $bp_active = in_array('buddypress/bp-loader.php', get_option('active_plugins'));
 
-      if ( current_user_can('activate_plugins') && ( $cg_active !== true || $bp_active !== true ) ) {
-        add_action('admin_notices', 'bpgts_plugin_admin_notice');
-      } else {
+      if ( current_user_can('activate_plugins') && ( $cg_active !== true && $bp_active !== true ) ) {
+        add_action('admin_notices', 'bpgts_both_plugin_admin_notice');
+      } 
+	  else if ( current_user_can('activate_plugins') && ( $cg_active !== true ) ) {
+        add_action('admin_notices', 'bpgts_bp_create_group_type_plugin_admin_notice');
+      }
+	  else if ( current_user_can('activate_plugins') && ( $bp_active !== true ) ) {
+        add_action('admin_notices', 'bpgts_bp_plugin_admin_notice');
+      }
+	  else {
         if (!defined('BPGTS_PLUGIN_BASENAME')) {
             define('BPGTS_PLUGIN_BASENAME', plugin_basename(__FILE__));
         }
@@ -88,12 +101,32 @@ function bpgts_bpcg_settings_link( $links ) {
 }
 
 // Throw an Alert to tell the Admin why it didn't activate
-function bpgts_plugin_admin_notice() {
+function bpgts_both_plugin_admin_notice() {
     $bpcg_plugin = "BP Group Type Search";
     $bp_plugin = "BuddyPress";
-    $cg_plugin = "BP Add Group Types";
+    $cg_plugin = "BP Create Group Types";
     echo '<div class="error"><p>'
     . sprintf(__('%1$s requires %2$s and %3$s to function correctly. Please activate %2$s and %3$s before activating %1$s.', BPGTS_TEXT_DOMAIN), '<strong>' . esc_html($bpcg_plugin) . '</strong>', '<strong>' . esc_html($bp_plugin) . '</strong>', '<strong>' . esc_html($cg_plugin) . '</strong>')
+    . '</p></div>';
+    if (isset($_GET['activate']))
+        unset($_GET['activate']);
+}
+
+function bpgts_bp_create_group_type_plugin_admin_notice() {
+    $bpgts_plugin = "BP Group Type Search";    
+    $bpcgt_plugin = "BP Create Group Types";
+    echo '<div class="error"><p>'
+    . sprintf(__('%1$s requires %2$s to function correctly. Please activate %2$s before activating %1$s.', BPGTS_TEXT_DOMAIN), '<strong>' . esc_html($bpgts_plugin) . '</strong>' , '<strong>' . esc_html($bpcgt_plugin) . '</strong>')
+    . '</p></div>';
+    if (isset($_GET['activate']))
+        unset($_GET['activate']);
+}
+
+function bpgts_bp_plugin_admin_notice() {
+    $bpgts_plugin = "BP Group Type Search"; 
+    $bp_plugin = "BuddyPress";
+    echo '<div class="error"><p>'
+    . sprintf(__('%1$s requires %2$s to function correctly. Please activate %2$s before activating %1$s.', BPGTS_TEXT_DOMAIN), '<strong>' . esc_html($bpgts_plugin) . '</strong>' , '<strong>' . esc_html($bp_plugin) . '</strong>')
     . '</p></div>';
     if (isset($_GET['activate']))
         unset($_GET['activate']);
